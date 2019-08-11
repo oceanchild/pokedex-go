f59064040d0c410263754f75406ca10d15a02ea6
Jacky Shao <jacky.shaows@gmail.com>
2019-08-11 23:19:40 +0200

    Add content on pokemons page

---
+++ src/components/PokemonDescription.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="pokemon__content pokemon__content--description">
+    <h1 class="pokemon__title">Description</h1>
+    <div class="pokemon__text">{{ pokemon.description }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["pokemon"]
+};
+</script>
---
+++ src/components/PokemonEvolutions.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="pokemon__content pokemon__content--evolutions">
+    <h1 class="pokemon__title">Évolutions</h1>
+    <ul class="pokemon__list">
+      <li
+        v-for="evolution in pokemon.evolutions"
+        :key="evolution.value"
+        class="pokemon__item pokemon__item--evolutions"
+      >
+        <img :src="getImage(evolution.id)" :alt="evolution.name" class="pokemon__image" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["pokemon"],
+  methods: {
+    getImage(id) {
+      return require(`@/assets/images/${id}.png`);
+    }
+  }
+};
+</script>
